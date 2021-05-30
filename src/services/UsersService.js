cbdf8b8e80f4dee993761025083180bd5f53b580
Yuriy Panko <yuriy.panko@gmail.com>
2021-05-30 17:14:21 +0300

    feature/manageUsers
    ### feature
    - connect by sockets with unique name
    - get list of users
    - get notifications about user online status

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,8 +1,11 @@
+import { v4 as uuid } from 'uuid';
+
 import { paginateList } from '../utils/helpers/PaginateList';
+import { generateName } from '../utils/helpers/NameGenerator';
 
 export default class UsersService {
   constructor() {
-    this.users = [];
+    this.users = {};
   }
 
   /**
@@ -11,13 +14,37 @@ export default class UsersService {
    * @param {number} query.offset
   */
   getList(query) {
-    return paginateList(this.users, query);
+    return paginateList(Object.values(this.users), query);
   }
 
   /**
    * Get count of users
   */
-  get getCount() {
-    return this.users.length;
+  get count() {
+    return Object.values(this.users).length;
+  }
+
+  /**
+   * Add new user
+   * @param socketId
+  */
+  handleUserCreation(socketId) {
+    const model = {
+      id: uuid(),
+      socketId,
+      name: generateName(Object.values(this.users).map(({ name } = {}) => name)),
+      onCall: false,
+    };
+
+    this.users[model.id] = model;
+    return model;
+  }
+
+  /**
+   * Delete user by id
+   * @param id
+  */
+  deleteUser(id) {
+    delete this.users[id];
   }
 }
